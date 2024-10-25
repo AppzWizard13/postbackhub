8ad66e0191d40e78f7f6cde0c449ed38dabf844a
appz13 <appzwizard13@gmail.com>
2024-10-26 00:39:06 +0530

    auto sl cron set for 2 seconds

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -239,14 +239,14 @@ def get_pending_order_filter_dhan(response):
 def start_scheduler():
     scheduler = BackgroundScheduler()
 
-    # Self-ping every 58 seconds
-    # scheduler.add_job(self_ping, IntervalTrigger(seconds=58))
-    # scheduler.add_job(auto_order_count_monitoring_process, IntervalTrigger(seconds=10))
-    # # Restore user kill switches every Monday to Friday at 4:00 PM
-    # scheduler.add_job(restore_user_kill_switches, CronTrigger(day_of_week='mon-fri', hour=16, minute=0))
+    Self-ping every 58 seconds
+    scheduler.add_job(self_ping, IntervalTrigger(seconds=58))
+    scheduler.add_job(auto_order_count_monitoring_process, IntervalTrigger(seconds=10))
+    # Restore user kill switches every Monday to Friday at 4:00 PM
+    scheduler.add_job(restore_user_kill_switches, CronTrigger(day_of_week='mon-fri', hour=16, minute=0))
 
     # for Test 
-    scheduler.add_job(autoStopLossProcessing, IntervalTrigger(seconds=10))
+    scheduler.add_job(autoStopLossProcessing, IntervalTrigger(seconds=2))
     scheduler.start()
     print("INFO: Scheduler started.")
 
