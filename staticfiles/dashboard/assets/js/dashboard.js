d57b7883f1377d8b209d5d059a591b04631eacc3
appz13 <appzwizard13@gmail.com>
2024-11-07 10:08:12 +0530

    ui chart color change

--- staticfiles/dashboard/assets/js/dashboard.js
+++ staticfiles/dashboard/assets/js/dashboard.js
@@ -11,17 +11,18 @@ $(function () {
       var parsedSeriesData = JSON.parse(breakup_seriesdata);
       var breakup_series = parsedSeriesData.map(num => Math.round(num));
     
+      // Set colors based on the second value's positivity/negativity
+      var colors = [
+        "#3267ff", // First color - Blue (default)
+        breakup_series[1] > 0 ? "#4cb849" : "#fa2d2d", // Green if positive, Red if negative
+        "#fff47d" // Third color - Orange (default)
+      ];
       // Check if the second value is negative and make it positive if so
       if (breakup_series[1] < 0) {
         breakup_series[1] = Math.abs(breakup_series[1]);
       }
       
-    // Set colors based on the second value's positivity/negativity
-    var colors = [
-      "#3267ff", // First color - Blue (default)
-      breakup_series[1] > 0 ? "#4cb849" : "#fa2d2d", // Green if positive, Red if negative
-      "#fff47d" // Third color - Orange (default)
-    ];
+
     
     } catch (e) {
       console.error("Failed to parse breakup_seriesdata as JSON:", e);
