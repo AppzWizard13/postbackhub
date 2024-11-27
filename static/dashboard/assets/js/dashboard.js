e66a8d0b30a6f31472aa9842aed9fb0e61dcc42a
appz13 <appzwizard13@gmail.com>
2024-11-27 15:24:24 +0530

    UI chnages

--- static/dashboard/assets/js/dashboard.js
+++ static/dashboard/assets/js/dashboard.js
@@ -172,7 +172,7 @@ $(function () {
 
         chart: {
             type: "bar",
-            height: 500,
+            height: 550,
             offsetX: -15,
             toolbar: { show: true },
             foreColor: "#000000d9",
